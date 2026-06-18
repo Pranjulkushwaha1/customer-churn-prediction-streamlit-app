0e2bbd83895df1012098a243f3dc6effd8e0a059
Pranjul <kushwahapranjul062@gmail.com>
2026-06-18 08:11:50 +0530

    Add Prediction Logic

--- app.py
+++ app.py
@@ -36,3 +36,59 @@ satisfaction_score = st.slider("Satisfaction Score",
 offer = st.selectbox("Offer",
                       ['No Offer', 'Offer A', 'Offer B', 
                        'Offer C', 'Offer D', 'Offer E'])
+
+# Ab hm prediction Logic likhenge
+
+# prediction ki button
+if st.button("Predict Churn"):
+
+    # Step 1 41 column ko zeros ka array banana hai.
+    input_data = np.zeros(41)
+
+    # Step 2 User Input shi postion me dalo.
+    input_data[5] = number_of_referrals
+    input_data[6] = tenure
+    input_data[21] = monthly_charge
+    input_data[23] = satisfaction_score
+
+    # Step 3 Offer Encoading
+    offer_mapping = {
+        'No Offer':25,'Offer A':26, 'Offer B':27,
+        'Offer C':28, 'Offer D':29, 'Offer E':30
+    }
+    input_data[offer_mapping[offer]] = 1
+
+    # step 4 Internet type Encoding
+    internet_mapping = {
+        'Cable':31, 'DSL':32,
+        'Fiber Optic':33, 'No Internet':34
+    }
+    input_data[internet_mapping[internet_type]] = 1
+
+    # Step COntract Encoading
+    Contract_mapping = {
+        'Month-to-Month':35,
+        'One Year':36,
+        'Two Year':37
+    }
+    input_data[Contract_mapping[contract]] = 1
+
+    # Step 6 Payment method encoading
+    payment_mapping = {
+        'Bank Withdrawal':38,
+        'Credit Card':39,
+        'Mailed Check':40
+    }
+    input_data[payment_mapping[payment_method]] = 1
+    
+    # Step 7 — Scale karo
+    input_scaled = scaler.transform([input_data])
+    
+    # Step 8 — Predict karo
+    prediction = model.predict(input_scaled)[0]
+    
+    # Step 9 — Result dikhao
+    if prediction == 1:
+        st.error("⚠️ Customer Churn Karega!")
+    else:
+        st.success("✅ Customer Nahi Jaayega!")
